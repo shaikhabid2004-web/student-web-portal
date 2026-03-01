1f002258fae69a900ac03fa982df22bd1df383c2
shaikhabid2004-web <shaikhabid2004@gmail.com>
2026-03-01 13:43:43 +0530

    Use environment variable for port configuration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
